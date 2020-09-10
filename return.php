30acb9f6ee3103e461fa9c0bc3dd9ea6d7d05ca8
Felipe Guimarães <fguiengenharia@gmail.com>
2020-09-10 00:04:25 -0300

    DevNotes API (estrutural): Update

--- return.php
+++ return.php
@@ -2,7 +2,7 @@
 //Permitindo requisições AJAX externas, para minha API
 header("Access-Control-Allow-Origin: http://resttesttest.com");
 //Permitindo os métodos de requisição para a minha API
-header("Access-Control-Allow-Methods: GET, POST, INSERT");
+header("Access-Control-Allow-Methods: GET, POST, PUT");
 //O tipo de retorno da minha API sempre será no formato JSON
 header("Content-Type: apllication/json");
 
